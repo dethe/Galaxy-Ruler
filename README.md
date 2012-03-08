7a2a12d25d57a8dabd59ef7699fda493a77864ac
delza <delza@livingcode.org>
2012-03-07 22:56:09 -0800

    added hexmap to default server page, todo list, basic ship drawing

--- README.md
+++ README.md
@@ -11,3 +11,15 @@ Galaxy ruler is a space themed MMO strategy game where you build your empire, co
 ## Planets
 
 # ETC.
+
+# ToDo
+
+* Register a new player
+* Store players for next time (db and cookie)
+* Set up database
+* Draw player ship(s)
+* Move ship
+* Send movement to other players
+* Web page to input / edit database info
+* Display hex info (planets, etc.)
+* Code to move in infinite (or at least very large) space
--- js/simplegame.js
+++ js/simplegame.js
@@ -3,3 +3,21 @@ var socket = io.connect('http://localhost:9999');
     console.log(data);
     socket.emit('my other event', { my: 'data' });
  });
+
+var degree = Math.PI / 180;
+
+function drawShip(color, pos, rot){
+	ctx.save();
+	ctx.translate(pos.x, pos.y);
+	ctx.rotate(-(rot * degree));
+	ctx.fillStyle = color;
+	ctx.beginPath();
+	ctx.moveTo(20,0);
+	ctx.lineTo(-20,15);
+	ctx.lineTo(-5,0);
+	ctx.lineTo(-20,-15);
+	ctx.closePath();
+	ctx.fill();
+	ctx.restore();
+}
+
